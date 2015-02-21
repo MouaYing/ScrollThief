8d7bfb3b8fa68467cb7f64b1f205a372c9e0f2f5
Kevin Hinton <KevinHinton@outlook.com>
2015-02-21 07:56:32 -0700

    small xbox controller bug fix

--- src/scrollthief/controller/XboxAdapter.java
+++ src/scrollthief/controller/XboxAdapter.java
@@ -37,7 +37,7 @@ public class XboxAdapter extends XboxControllerAdapter{
 		gameControl.setCameraDirection(direction);
 		if(direction > 300 || direction < 60)
 			gameControl.switchSelectedButton(0);
-		else if(direction > 120 || direction < 210)
+		else if(direction > 120 && direction < 210)
 			gameControl.switchSelectedButton(1);
 	}
 	
